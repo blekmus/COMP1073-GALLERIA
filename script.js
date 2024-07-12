d1e36d1aa54eea72c529cc83011630043b622159
blekmus <dinil@protonmail.com>
2024-07-12 14:58:33 -0400

    finished

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+// Add an event listener to the gallery list
 function handleClick(element, figure) {
+  // Get all the children of the element
   const children = element.children;
+  // Loop through each child
   for (let i = 0; i < children.length; i++) {
+    // Add an event listener to each child
     children[i].addEventListener('click', function() {
       for (let j = 0; j < children.length; j++) {
         if (children[j] === children[i]) {
+          // Add the active class to the clicked child
           children[j].classList.add('active');
+
+          // Set the image source and title of the figure element
           figure.children[0].src = children[j].getAttribute('image-src');
           figure.children[1].textContent = children[j].getAttribute('image-title');
         } else {
+          // Remove the active class from the other children
           children[j].classList.remove('active');
         }
       }
@@ -15,18 +23,28 @@ function handleClick(element, figure) {
   }
 }
 
+// Add elements to the gallery list
 function addElements(imageList, galleryList) {
+  // Loop through each image in the image list
   imageList.forEach(image => {
+    // Create new elements
     let li = document.createElement('li');
     let img = document.createElement('img');
     img.src = image.small;
+
+    // Set the attributes of the elements to track large image and title
     li.setAttribute('image-src', image.large)
     li.setAttribute('image-title', image.title)
+
+    // Append the elements to the list item
     li.append(img);
+
+    // Append the list item to the gallery list
     galleryList.append(li);
   })
 }
 
+// Image links
 const imageLinks = [
   {
     title: "Pink flowers",
@@ -55,10 +73,18 @@ const imageLinks = [
   },
 ]
 
+// Get the gallery list and figure elements
 const galleryList = document.getElementById('gallery-list')
 const figure = document.getElementById('gallery-figure')
+
+// Add the image links to the gallery list
 addElements(imageLinks, galleryList);
+
+// Add an event listener to the gallery list
 handleClick(galleryList, figure);
 
+// Set the first image as active
 galleryList.children[0].classList.add('active')
+
+// Set the image source and title of the figure element
 figure.children[1].textContent = imageLinks[0].title
